e512c9c788253bd40d31ab9c694402e80b34217a
AbolnikovAnton <bakunin.abolnikov@gmail.com>
2022-04-29 14:08:40 +0300

    change something

--- src/App.js
+++ src/App.js
@@ -1,32 +1,38 @@
-import React, {useEffect, useState} from 'react';
-import './App.css';
-import Header from "./components/ui/Header";
-import axios from "axios";
-import EpisodesGrid from "./components/episodes/EpisodesGrid";
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Header from './components/ui/Header'
+import CharacterGrid from './components/characters/CharacterGrid'
+import Search from './components/ui/Search'
+import './App.css'
 
 const App = () => {
-    const [episods, setEpisods] = useState([])
-    const [character, setCharacter] = useState([])
-    const [isLoading, setIsLoading] = useState(true)
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [query, setQuery] = useState('')
 
-    useEffect(() => {
-        const fetchItems = async () => {
-            const episodsData = await axios('https://breakingbadapi.com/api/episodes')
-            const characterData = await axios('https://breakingbadapi.com/api/characters')
+  useEffect(() => {
+    const fetchItems = async () => {
+      setIsLoading(true)
+      const result = await axios(
+        `https://www.breakingbadapi.com/api/characters?name=${query}`
+      )
 
-            setEpisods(episodsData.data)
-            setCharacter(characterData.data)
-            setIsLoading(false)
-        }
-        fetchItems()
-    }, [])
+      console.log(result.data)
 
-    return (
-        <div className='container'>
-            <Header />
-            <EpisodesGrid isLoading={isLoading} episods={episods}/>
-        </div>
-    );
+      setItems(result.data)
+      setIsLoading(false)
+    }
+
+    fetchItems()
+  }, [query])
+
+  return (
+    <div className='container'>
+      <Header />
+      <Search getQuery={(q) => setQuery(q)} />
+      <CharacterGrid isLoading={isLoading} items={items} />
+    </div>
+  )
 }
 
-export default App;
+export default App
